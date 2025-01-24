89c3f8179910d841f647e49dc1a85b930c78df87
robert <Neorganizm911@yandex.ru>
2025-01-24 23:58:46 +0300

    second step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
