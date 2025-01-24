12401c738dd0cd5adcbaf1311dd309e26eba5483
robert <Neorganizm911@yandex.ru>
2025-01-24 22:52:54 +0300

    first step

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-# Устанавливаем uv, если он ещё не установлен
-if ! command -v uv &> /dev/null; then
-    curl -LsSf https://astral.sh/uv/install.sh | sh
-    source $HOME/.local/bin/env
-fi
-
-# Добавляем uv в PATH
-export PATH="$HOME/.local/bin:$PATH"
-
-# Устанавливаем зависимости
-uv sync
+# скачиваем uv и запускаем команду установки зависимостей
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+make install
